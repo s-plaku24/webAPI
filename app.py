0f870da84944f1197a3b080a5b7fc52fee065132
s-plaku24 <s_plaku24@stud.hwr-berlin.de>
2025-07-14 17:42:36 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🚀 ISS Tracker - Test")
+st.write("App is working!")
+
+# Simple test without external APIs
+st.write("This is a test to see if the app loads properly.")
